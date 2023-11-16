17b0cdeafc657e618696a8957b6155ec00a20842
Anush Ravishankar <92255303+anushravishankar@users.noreply.github.com>
2023-11-16 20:15:46 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
+
+import { env } from '../environments/environments';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const env = {
+    production: false,
+    firebase : {
+        apiKey: "AIzaSyBPk8TKquyEezU7MgVs6hu3y30iozoKxM0",
+        authDomain: "tictactoe9989.firebaseapp.com",
+        projectId: "tictactoe9989",
+        storageBucket: "tictactoe9989.appspot.com",
+        messagingSenderId: "488273757077",
+        appId: "1:488273757077:web:de359016514527fda2ac9c",
+        measurementId: "G-BY5ZSE6XJW"
+      }
+};
